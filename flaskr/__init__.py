a0f299435fa0f778afbdf393507576be3de41b0b
Roman Halychanivkyi <30769247+Romko97@users.noreply.github.com>
2020-08-07 22:20:25 +0300

    Update __init__.py
    add HOST flasker-pkg.herokuapp.com

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,6 +4,7 @@ def create_app(test_config=None):
     #creat and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
+        ALLOWED_HOSTS='flasker-pkg.herokuapp.com',
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
         )
